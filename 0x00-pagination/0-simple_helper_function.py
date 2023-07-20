50055506e705b98212c566a9e889e6217543508f
Manuel254 <Immanuelkituku@gmail.com>
2023-07-20 17:42:32 +0300

    Add helper function for pagination

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Module that has a simple helper function for handling pagination"""
+from typing import Tuple
+
+
+def index_range(page: int, page_size: int, **kwargs: int) -> Tuple[int, int]:
+    """Returns a tuple containing the start index and end index
+    corresponding to the range of indexes to return in a list
+    for those particular pagination parameters"""
+    for i in range(1, page + 1):
+        if i == page:
+            start = page_size * (i - 1)
+            stop = start + page_size
+    return (start, stop)
