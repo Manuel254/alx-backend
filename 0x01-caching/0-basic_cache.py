355213933184a71abb7cd2ab4fea861423df9926
Manuel254 <immanuelkituku@gmail.com>
2023-07-25 18:43:59 +0000

    Fix bugs

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -17,4 +17,6 @@ class BasicCache(BaseCaching):
 
     def get(self, key):
         """Get an item by key"""
+        if key is None or key not in self.cache_data.keys():
+            return None
         return self.cache_data.get(key, None)
