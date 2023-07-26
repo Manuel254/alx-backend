69d9695974ca61c02417f1f0ce848579dabb6342
Manuel254 <immanuelkituku@gmail.com>
2023-07-26 10:45:50 +0000

    Fix bug

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -15,13 +15,12 @@ class FIFOCache(BaseCaching):
         if key is None or item is None:
             pass
         else:
-            if len(self.cache_data) >= BaseCaching.MAX_ITEMS \
-                    and key not in self.cache_data.keys():
-                first_key = next(iter(self.cache_data))
-                self.cache_data.pop(first_key)
-                print("DISCARD: {}".format(first_key))
             self.cache_data[key] = item
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            first_key = next(iter(self.cache_data))
+            self.cache_data.pop(first_key)
+            print("DISCARD: {}".format(first_key))
 
     def get(self, key):
         """Get an item from cache"""
-        return self.cache_data(key)
+        return self.cache_data.get(key)
