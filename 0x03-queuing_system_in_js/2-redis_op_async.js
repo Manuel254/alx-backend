a86d520913caa3ff47b6b71058cbbc5beb753375
Manuel254 <immanuelkituku@gmail.com>
2023-08-29 07:48:35 +0000

    Promisify callback

---
+++ 0x03-queuing_system_in_js/2-redis_op_async.js
@@ -0,0 +1,28 @@
+import { createClient, print } from 'redis';
+import { promisify } from 'util';
+
+const client = createClient();
+
+client.on('error', err => console.log(`Redis client not connected to the server: ${err}`));
+
+client.on('connect', () => console.log('Redis client connected to the server'));
+
+function setNewSchool(schoolName, value) {
+  client.set(schoolName, value, print);
+}
+
+const get = promisify(client.get).bind(client);
+
+async function displaySchoolValue(schoolName) {
+  try {
+    const value = await get(schoolName);
+    console.log(value);
+    }
+  catch (err) {
+    throw err;
+  }
+}
+
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
