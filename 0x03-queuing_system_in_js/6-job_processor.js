31f26e3637d5a7727674c36381a87f96765f69e3
Manuel254 <immanuelkituku@gmail.com>
2023-08-30 15:13:59 +0000

    Add file that processes job data

---
+++ 0x03-queuing_system_in_js/6-job_processor.js
@@ -0,0 +1,14 @@
+const kue = require('kue');
+
+const queue = kue.createQueue();
+
+
+function sendNotification(phoneNumber, message) {
+  console.log(`Sending notification to ${phoneNumber}, with message: ${message}`);
+}
+
+queue.process('push_notification_code', (job) => {
+  const phoneNumber = job.data.phoneNumber;
+  const message = job.data.message;
+  sendNotification(phoneNumber, message);
+});
